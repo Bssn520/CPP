66c341295dddd5bbf18ab76d86d431f77d95ddd6
Bssn520 <bssn007@outlook.com>
2024-10-22 16:48:15 +0800

    learn: 构造函数 默认构造函数 default 在类的外部定义构造函数

---
+++ 7.类/1.定义抽象数据类型/Person_ex15.h
@@ -0,0 +1,44 @@
+#ifndef PERSON_H_
+#define PERSON_H_
+
+#include <string>
+#include <iostream>
+
+/** Person 类 */
+struct Person;
+/** 读取一个 Person 实例 */
+std::istream &read(std::istream &is, Person &item);
+/** 打印一个 Person 实例 */
+std::ostream &print(std::ostream &os, const Person &item);
+
+
+struct Person
+{
+    // 构造函数
+    Person() : name(""), address("") { }    // Person() : default;
+    Person(const std::string &sname, const std::string &saddress) : name(sname), address(saddress) { }
+    Person(std::istream &is) { read(is, *this); }
+    // 成员函数
+    std::string get_name() const { return name; }
+    std::string get_address() const { return address; }
+    // 数据成员
+    std::string name;
+    std::string address;
+};
+
+/** 读取一个 Person 实例 */
+std::istream &read(std::istream &is, Person &item)
+{
+    is >> item.name >> item.address;
+
+    return is;
+}
+/** 打印一个 Person 实例 */
+std::ostream &print(std::ostream &os, const Person &item)
+{
+    os << item.name << " " << item.address;
+
+    return os;
+}
+
+#endif
