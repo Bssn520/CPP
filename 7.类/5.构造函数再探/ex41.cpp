ebaed01d5728369c2cbab5b5353c4cf7793f130a
Bssn520 <bssn007@outlook.com>
2024-10-26 17:37:03 +0800

    learn: 构造函数 构造函数的默认实参 自动生成默认构造函数的条件 委托构造函数 类类型转换 聚合类 constexpr构造函数

---
+++ 7.类/5.构造函数再探/ex41.cpp
@@ -0,0 +1,14 @@
+#include "Sales_data_ex41.h"
+#include <iostream>
+
+
+int main()
+{
+    Sales_data first;   // 2
+
+    Sales_data second("299200-3920-2929-3902"); // 3
+
+    Sales_data third(std::cin); // 2 4
+
+    return  0;
+}
