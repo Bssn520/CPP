5ada1575f50b922207ed510d76a6c286d4df30a9
花海 の Mac <bssn@MacBook-Air.local>
2024-10-31 21:32:21 +0800

    fix: 错误的代码块范围

--- 8.IO库/1.IO类/ex06.cpp
+++ 8.IO库/1.IO类/ex06.cpp
@@ -1,6 +1,17 @@
+/*
+ * @Author: error: git config user.name & please set dead value or install git
+ * @Date: 2024-10-31 11:37:56
+ * @LastEditTime: 2024-10-31 21:31:29
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by Bssn520, All Rights Reserved. 
+ */
+
 #include "Sales_data_ex41.hpp"
 #include <fstream>
 #include <iostream>
+#include <ostream>
+
 
 int main(int argc, char *argv[]) {
     if (argc <= 1 || argc > 3) {// 参数检查
@@ -13,7 +24,7 @@ int main(int argc, char *argv[]) {
         std::cerr << "Error: Could not open input file " << argv[1] << std::endl;
         return 1;
     }
-    
+
     std::ofstream fileOutput;
     std::ostream *output = &std::cout;
     if (argc == 3) {// 如果提供了输出文件路径则尝试打开
@@ -21,26 +32,25 @@ int main(int argc, char *argv[]) {
         if (!fileOutput) {
             std::cerr << "Error: Could not open output file " << argv[2] << std::endl;
             return 1;
-
-            output = &fileOutput;
         }
-        
-        Sales_data total;
-        if (read(input, total)) {
-            Sales_data trans;
-            while (read(input, trans)) {
-                if (total.isbn() == trans.isbn()) {
-                    total.combine(trans);
-                } else {
-                    print(*output, total) << std::endl;
-                    total = trans;
-                }
-            }
-            print(*output, total) << std::endl;
-        } else {
-            std::cerr << "No Data?!" << std::endl;
-        }
+        output = &fileOutput;
     }
-    
+
+    Sales_data total;
+    if (read(input, total)) {
+        Sales_data trans;
+        while (read(input, trans)) {
+            if (total.isbn() == trans.isbn()) {
+                total.combine(trans);
+            } else {
+                print(*output, total) << std::endl;
+                total = trans;
+            }
+        }
+        print(*output, total) << std::endl;
+    } else {
+        std::cerr << "No Data?!" << std::endl;
+    }
+
     return 0;
 }
